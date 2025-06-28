445b00d704f48aadb6dfbe49fe066a2343369b3e
Julian <jmguintu3@student.hau.edu.ph>
2025-06-28 12:59:08 +0800

    updated files

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const imageUrls = [
+  'mark-zuckerberg.png',
+  'elon-musk.png',
+];
+
+
+
+
+function getRandomImage() {
+  const index = Math.floor(Math.random() * imageUrls.length);
+  return imageUrls[index];
+}
+
+function App() {
+  const [images, setImages] = useState(Array(36).fill().map(getRandomImage));
+
+  const handleClick = () => {
+    setImages(images.map(() => getRandomImage()));
+  };
+
+
+
+  return (
+    
+    <div className="container">
+      <h1> ZuckMusk Game!</h1>
+      
+      <div className="grid">
+        {images.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt="Random"
+            className="square"
+            onClick={handleClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
